2ef86405c4826d0762dce226a25decdf08c9b715
Esteban MANSART <esteban.mansart@gmail.com>
2022-08-18 15:35:41 +0200

    Ajout de webpack

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path")
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    resolve: {
+        fallback: {
+            fs: false,
+            path: false
+        },
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9000,
+        host: "0.0.0.0",
+        allowedHosts: 'all',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+
+            {
+                test: /\.css$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                ],
+            },
+        ],
+    }
+}
